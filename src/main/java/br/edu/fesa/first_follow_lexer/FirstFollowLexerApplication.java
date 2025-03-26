f449eb366d647a990a03505218a41671c4b4f244
Roger Freitas Pereira <roger_freitas@live.com>
2025-03-26 06:57:50 -0300

    Initial Project Setup with Spring Boot 3.4.4
    This initial commit establishes the project structure generated via
    Spring Initializr using Java 17 and Maven. It includes essential
    dependencies such as Spring Web, Lombok, Spring Boot DevTools, and
    Thymeleaf, providing a solid foundation for the application development.

---
+++ src/main/java/br/edu/fesa/first_follow_lexer/FirstFollowLexerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fesa.first_follow_lexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstFollowLexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstFollowLexerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fesa/first_follow_lexer/FirstFollowLexerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fesa.first_follow_lexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstFollowLexerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
